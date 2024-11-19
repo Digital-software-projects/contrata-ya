4ac7fb0d3e970ae7980c4fe1b8bbee8f9627c0d9
just-fabian <fabian130404@gmail.com>
2024-11-19 00:45:19 -0400

    chore: change key role name in team

--- src/types/user/Team.ts
+++ src/types/user/Team.ts
@@ -5,7 +5,7 @@ export interface Team {
   name: string; // Team name (e.g., 'Development Team', 'Design Team')
   membersIds: string[]; // List of team members' ids (can be individuals or employees)
   members: {
-    [role: string]: string; // role of each member, e.g., abc2: UX designer
+    [memberId: string]: string; // role of each member, e.g., abc2: UX designer
   };
   teamLeadId?: string; // Optional team lead id
   description?: string; // Optional description of the team’s focus or mission
